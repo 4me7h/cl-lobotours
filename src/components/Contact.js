21a4af24fd8e899e69f195d6a005e1e3097719f6
4me7h <4573r10n@gmail.com>
2025-04-18 03:43:47 -0600

    component: footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import { Fade } from 'react-awesome-reveal';
 
 // import iconMail from '../images/icon-mail.png';
-import iconWhats from '../images/icon-whats.png';
+// import iconWhats from '../images/icon-whats.png';
 
 const Contact = () => {
     return(
@@ -17,25 +17,23 @@ const Contact = () => {
                             <div className="headercontent__header">
                                 <Fade cascade triggerOnce direction="up"> 
                                 <div className="about__header">
-                                    <div className="about__header__content">
+                                    <div className="about__header__content mb-5">
                                         <h2 className="text--subtitle text-center text-uppercase text--white">Contáctanos</h2>
                                         <p className='text--body text-center text-white'>
                                         Horario de atención: Lunes a Domingo, 8:00 am - 8:00 pm<br />
 ¡Escríbenos por WhatsApp para respuestas rápidas, cotizaciones o reservas!
                                         </p>
                                     </div>
+                                    <a className="btn btn--secondary" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">Reserva Aquí</a>
                                 </div>
-                                <ul className="contact__buttons">
-                                    {/* <li className='contact__buttons__item'>
-                                        <a className="contact__buttons__btn" target="__blank" href="mailto:blueprintstrategiesagency@gmail.com">
-                                            <img src={iconMail} alt="e-mail" className="contact__buttons__icon"/>
-                                        </a></li> */}
-                                    <li className="contact__buttons__item">
-                                        <a className="contact__buttons__btn" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">
-                                            <img src={iconWhats} alt="whatsapp" className="contact__buttons__icon"/>
-                                        </a>
+                                {/* <ul className="contact__buttons">
+                                    <li className='contact__buttons__item'>
+                                        <a className="btn btn--secondary" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">Reserva Aquí</a>
                                     </li>
-                                </ul>
+                                    <li className="contact__buttons__item">
+                                        <a className="btn btn--secondary" target="__blank" href="https://wa.me/+5219613037177?text=Lobo%20Tours:%20Estoy%20interesado%20en%20un%20viaje%20de%20clase%20económica">Reserva Aquí</a>
+                                    </li>
+                                </ul> */}
                                 </Fade>
                             </div>
                         </div>
